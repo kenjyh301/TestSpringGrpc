86a4522dbec9626762a6958d30e17e2be3467b70
kenjyh301 <kenjyh301@gmail.com>
2021-05-16 23:23:45 +0700

    fix grpc client unimplement

--- GrpcClient/src/main/java/minh/project/GrpcClient/Service/GrpcService.java
+++ GrpcClient/src/main/java/minh/project/GrpcClient/Service/GrpcService.java
@@ -4,13 +4,13 @@ import com.google.protobuf.Empty;
 import io.grpc.Channel;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import minh.project.GrpcClient.Grpc.testGrpc;
+import minh.project.Grpc.testGrpc;
 
 public class GrpcService {
    public void run(){
         Channel channel= ManagedChannelBuilder.forTarget("127.0.0.1:5353").usePlaintext().build();
         testGrpc.testBlockingStub stub= testGrpc.newBlockingStub(channel);
 //        Empty empty;
-        stub.test(com.google.protobuf.Empty.getDefaultInstance());
+        stub.rPC1(Empty.getDefaultInstance());
     }
 }
